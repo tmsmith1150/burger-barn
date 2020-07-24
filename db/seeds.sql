4339e3740e0ad5e61fb9c170f3b91c2714685060
Michelle Jordan <tmsmith1150@live.com>
2020-07-24 17:56:54 -0400

    updating

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("jalapenoburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("jackburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bbqburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("deluxeburger", 0);
