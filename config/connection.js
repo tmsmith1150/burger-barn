f98b5b7e4962c5fa314dac6a9534435e1f328cb4
Michelle Jordan <tmsmith1150@live.com>
2020-07-28 14:34:16 -0400

    adding jawsdb-url

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,36 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Queen1tj",
-  database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Queen1tj",
+    database: "burgers_db"
+  });
+};
+
+connection.connect();
+
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Queen1tj",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
